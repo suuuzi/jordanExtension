ff99c9726bda1f30b75c6c399483fb9b6a1ad96f
Suzane Sant Ana <tetestonaldo@gmail.com>
2016-08-02 18:45:29 -0300

    Add files via upload

---
+++ jordan.js
@@ -0,0 +1,65 @@
+(function() {
+	
+	var main = function($) { 
+		
+		var self = $.ffExtension = new function(){};
+		
+		$.extend(self, {
+			jordanImages : [
+
+			],
+			handleImages : function (lstImgs, time)
+			{
+				$.each($('img'), function(i,item) { 
+					if($.inArray($(item).attr('src'), lstImgs) == -1)
+					{
+						var h = $(item).height();
+						var w = $(item).width();
+						
+						if(h > 0 && w > 0)
+						{
+							$(item).css('width', w + 'px').css('height', h + 'px');
+							$(item).attr('src', lstImgs[Math.floor(Math.random() * lstImgs.length)]); 
+						}
+						else
+						{
+							$(item).load(function(){
+									if($.inArray($(item).attr('src'), lstImgs) == -1)
+									{
+										var h = $(item).height();
+										var w = $(item).width();
+										$(item).css('width', w + 'px').css('height', h + 'px');
+										$(item).attr('src', lstImgs[Math.floor(Math.random() * lstImgs.length)]); 
+									}
+								});
+							}
+						}
+					});
+					
+					if(time > 0)
+						setTimeout(function(){self.handleImages(lstImgs, time);},time);
+				}
+			});
+
+		$(function(){
+			self.handleImages(self.jordanImages, 3000);
+		});
+	};
+
+	function loadJS(src, callback) {
+		var s = document.createElement('script');
+		s.src = src;
+		s.async = true;
+		s.onreadystatechange = s.onload = function() {
+			var state = s.readyState;
+			if (!callback.done && (!state || /loaded|complete/.test(state))) {
+				callback.done = true;
+				callback();
+			}
+		};
+		document.getElementsByTagName('head')[0].appendChild(s);
+	}
+	
+	main(jQuery);
+	
+ })();
